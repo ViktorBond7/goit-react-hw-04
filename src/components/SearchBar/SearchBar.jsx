9db48019486ab461d323b6b258c2f506faecb724
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-04-04 10:28:57 +0200

    repo

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+<header>
+  <form>
+    <input
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+    <button type="submit">Search</button>
+  </form>
+</header>;
